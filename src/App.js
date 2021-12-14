7d2325d73ec2876aa1f817e1c0c0f3f4cb4b38c3
Nik Asri <nikasri@icloud.com>
2021-12-14 15:02:42 +0800

    Integrated with Maps API

--- src/App.js
+++ src/App.js
@@ -1,58 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import Fab from '@mui/material/Fab';
+import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+
+export class MapContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="search-box">
+            <div id="title">Search Box</div>
+            <div id="pac-container"><input id="pac-input" type="text" placeholder="Enter a location" /></div>
+        </div>
+      <div>
+        <Map
+          google={this.props.google}
+          zoom={18}
+          style={mapStyles}
+          initialCenter={
+            {
+              lat: 3.157764,
+              lng: 101.711861
+            }
+          }
+        />
+      </div>
+      <div id="overlay">
+        {/* <input id="autocomplete" placeholder='Search' type="text" /> */}
+  
+        <Fab color="primary" size="medium" aria-label="search" variant="circular"><SearchIcon />
+        </Fab>
+      </div>
+      
+      </div>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCZeg__wAYOzCyQ4hr9IH_VArIV0vs5orY'
+})(MapContainer);
