136030180feb831f27fd071fd9b6ebc06d40b848
Nik Asri <nikasri@icloud.com>
2021-12-15 15:59:03 +0800

    Successful integration of Autocomplete API to suggest locations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+import { useRef, useState } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
-import Fab from '@mui/material/Fab';
-import SearchIcon from '@mui/icons-material/Search';
-import Button from '@mui/material/Button';
+import SearchBar from 'material-ui-search-bar';
+import Script from 'react-load-script';
 
 const mapStyles = {
   width: '100%',
@@ -11,18 +11,82 @@ const mapStyles = {
 
 
 export class MapContainer extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      location: '',
+      query: ''
+    };
+  }
+
+  handleScriptLoad = () => {
+    // Declare Options For Autocomplete
+    const options = {
+      // Location biasing to Malaysia
+      componentRestrictions: { country: "my" },
+      types: ['address'],
+      fields: ['address_components', 'geometry', 'name']
+      }
+
+    const input = document.getElementById('autocomplete');
+
+    // Initialize Google Autocomplete
+    this.autocomplete = new google.maps.places.Autocomplete(input,options);
+
+    // Avoid paying for data that you don't need by restricting the set of
+    // place fields that are returned to just the address components and formatted
+    // address.
+    this.autocomplete.setFields(['address_components', 'formatted_address']);
+    // Fire Event when a suggested name is selected
+    this.autocomplete.addListener('place_changed', this.handleSelection);
+  }
+
+
+  handleSelection = () => {
+    
+    // Should store the selected place history using Redux
+    const addressObject = this.autocomplete.getPlace();
+    const address = addressObject.address_components;
+
+    console.log(addressObject);
+    console.log(address);
+    // Check if address is valid
+    if (address) {
+      // Set State
+      this.setState(
+        {
+          location: address[0].long_name,
+          query: addressObject.formatted_address,
+        }
+      );
+    }
+  }
+
+  // handleMarker = () => {
+  //   const marker = new google.maps.Marker({map: map, draggable: false});
+  //   const location = "";
+  // }
+
   render() {
     return (
       <div>
-        <div className="search-box">
-            <div id="title">Search Box</div>
-            <div id="pac-container"><input id="pac-input" type="text" placeholder="Enter a location" /></div>
-        </div>
+        {/* <SearchBox /> */}
+        <SearchBar id="autocomplete" placeholder="Search a location"  value={this.state.query}/>
+        <Script
+          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZeg__wAYOzCyQ4hr9IH_VArIV0vs5orY&libraries=places"
+          onLoad={this.handleScriptLoad}
+        />
+        
       <div>
         <Map
           google={this.props.google}
-          zoom={18}
+          zoom={14}
           style={mapStyles}
+          fullscreenControl={true}
+          zoomControl={true}
+          streetViewControl={true}
+          // Set the map centre as KL
           initialCenter={
             {
               lat: 3.157764,
@@ -31,17 +95,30 @@ export class MapContainer extends React.Component {
           }
         />
       </div>
-      <div id="overlay">
-        {/* <input id="autocomplete" placeholder='Search' type="text" /> */}
-  
-        <Fab color="primary" size="medium" aria-label="search" variant="circular"><SearchIcon />
-        </Fab>
-      </div>
       
       </div>
     );
   }
 }
+
+// function renderAddress(place) {
+//   map.setCenter(place.geometry.location);
+//   marker.setPosition(place.geometry.location);
+//   marker.setVisible(true);
+// }
+const google = window.google;
+// const input = document.getElementById("autocomplete");
+
+// const options = {
+//   types: ['establishment'],
+//   fields: ['place_id', 'geometry', 'name']
+//   }
+
+// let autocomplete;
+
+// function initAutocomplete() {
+//   autocomplete = new google.maps.places.Autocomplete(input);
+// }
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCZeg__wAYOzCyQ4hr9IH_VArIV0vs5orY'
