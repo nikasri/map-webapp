2fa41714cf11e0e8e9e86cb02e807ceeba89904a
Nik Asri <nikasri@icloud.com>
2021-12-24 14:50:11 +0800

    Added material design and recent component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import MapContainer from "./component/MapContainer";
-import RecentComponent from "./component/RecentComponent"
 import { useSelector, useDispatch} from "react-redux";
 import { bindActionCreators } from "redux"
 import { actionCreators }  from "./state/index"
@@ -9,7 +8,6 @@ function App() {
 
   // Accessing data inside redux store
   const search = useSelector((state) => state.search);
-  const search2 = ["KUL", "KBR", "PEN"]
 
   console.log("Store: ",search)
   const dispatch = useDispatch();
@@ -18,7 +16,6 @@ function App() {
   const {searchAction} = bindActionCreators(actionCreators, dispatch)
   return (
     <div className="App">
-      <RecentComponent recent={search} />
       {/* Render MapContainer while also passing the searchAction action creator  as props */}
       <MapContainer action={searchAction}/>
 
